231d98e2e3ac1ec26999c7c1f3066466fe362db2
Ashish <ashish9gps@gmail.com>
2023-07-19 01:39:01 +0530

    Add files via upload
    Javascript fundamentals -Function-1
    final changes

---
+++ function-1.js
@@ -0,0 +1,42 @@
+// function without a parameter and argument
+
+//function declaration
+
+function sayMessage()
+{
+    console.log("Hey Everyone! I'am Happy to learn Full Stack Development fromPW Skills");
+}
+
+sayMessage();
+sayMessage();
+// sayMessage();
+// sayMessage();
+// sayMessage();
+
+
+
+//function returning value
+
+//declaration
+function passMessage()
+{
+    return "I am happy to learn development";
+}
+
+let Message = passMessage();
+
+console.log(Message);
+
+
+
+function passMessage()
+{
+    return "I am learning development!!";
+}
+
+//let message = passMessage();
+
+console.log(Message);
+
+let newMessage = Message + "Enroll Now";
+console.log(newMessage);
